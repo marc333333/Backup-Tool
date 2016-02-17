fc951106a2312c78e8f299acd719ee0d52476ea6
marc333333 <h2obobomaxhp@gmail.com>
2016-02-17 18:10:08 -0500

    Petits ajouts (non-complet)

---
+++ BackupTool/BackupTool/CBackup.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace BackupTool
+{
+    public static class CBackup
+    {
+        public static bool doBackup(string _strSourcePath, string _strDestinationPath)
+        {
+            if (_strSourcePath.Last() == '/') _strSourcePath.Remove(_strSourcePath.Length - 1);
+            if (_strDestinationPath.Last() == '/') _strDestinationPath.Remove(_strDestinationPath.Length - 1);
+
+            if (Directory.Exists(_strSourcePath) && Directory.Exists(_strDestinationPath))
+            {
+                SQLiteConnection db;
+                if (!File.Exists(_strDestinationPath + "/hashes.sqlite3"))
+                {
+                    SQLiteConnection.CreateFile(_strDestinationPath + "/");
+                    db = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                    db.Open();
+                }
+                else
+                {
+                    db = new SQLiteConnection("Data Source=MyDatabase.sqlite;Version=3;");
+                    db.Open();
+                }
+
+                db.Clone();
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- BackupTool/BackupTool/Form1.Designer.cs
+++ BackupTool/BackupTool/Form1.Designer.cs
@@ -28,12 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtSource = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileName = "w";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(85, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Dossier Source :";
+            // 
+            // txtSource
+            // 
+            this.txtSource.Location = new System.Drawing.Point(122, 41);
+            this.txtSource.Name = "txtSource";
+            this.txtSource.Size = new System.Drawing.Size(100, 20);
+            this.txtSource.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(443, 261);
+            this.Controls.Add(this.txtSource);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "BackupTool";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtSource;
     }
 }
 
